ca91f245ab1512a15f746afee7206fe888fecd3f
Aris Papadopoulos <aris.ppd@gmail.com>
2020-04-10 18:36:08 +0300

    Updates

---
+++ precache-manifest.f8315bf7029ed8eca9576a91d731ae73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7e5b769f45902e34ae74ae64ea43bb",
+    "url": "/codenames-key-generator/index.html"
+  },
+  {
+    "revision": "81310aeb2df428211205",
+    "url": "/codenames-key-generator/static/css/main.e3ecf5b8.chunk.css"
+  },
+  {
+    "revision": "e8016b82af5d27b43322",
+    "url": "/codenames-key-generator/static/js/2.0650b455.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codenames-key-generator/static/js/2.0650b455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81310aeb2df428211205",
+    "url": "/codenames-key-generator/static/js/main.c8a838ed.chunk.js"
+  },
+  {
+    "revision": "91d353be57d887ce1517",
+    "url": "/codenames-key-generator/static/js/runtime-main.8b6b46e0.js"
+  },
+  {
+    "revision": "90595896067c8dca8953210ba731be3e",
+    "url": "/codenames-key-generator/static/media/greece.90595896.svg"
+  },
+  {
+    "revision": "9d579f759224e6fec1a7fdc0fa56701e",
+    "url": "/codenames-key-generator/static/media/main.9d579f75.png"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/codenames-key-generator/static/media/uk.e5564902.svg"
+  }
+]);
